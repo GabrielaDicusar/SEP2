0317299c200765e7cb5c37b6f5a1e3cea0d84af0
dianasuzana <dianastratan@gmail.com>
2022-12-05 17:51:28 +0100

    Register View was created and data base implementation updated

--- src/server/mediator/AccountDB/AccountDAO.java
+++ src/server/mediator/AccountDB/AccountDAO.java
@@ -5,7 +5,6 @@ package server.mediator.AccountDB;
 public interface AccountDAO
 {
 
-  String addAccount(String username, String password, String firstname,
-      String lastname, String email, String phonenumber);
+  String addAccount(String firstname, String lastname, String email, String phonenumber, int account_type, String username, String password);
 }
 
--- src/server/mediator/AccountDB/AccountDAOImpl.java
+++ src/server/mediator/AccountDB/AccountDAOImpl.java
@@ -4,9 +4,16 @@ import server.mediator.ConnectionDB;
 
 
 import java.sql.*;
-
+/**
+ * @author dianasuzana
+ * @version 1.0
+ */
 public class AccountDAOImpl implements AccountDAO
 {
+
+  /**
+   * A constructor that will initialize the with the db driver
+   */
   public AccountDAOImpl()
   {
     try {
@@ -15,24 +22,25 @@ public class AccountDAOImpl implements AccountDAO
       e.printStackTrace();
     }
   }
-  @Override public String addAccount(String username, String password,
-      String firstname, String lastname, String email, String phonenumber)
+
+  @Override public String addAccount(String firstname, String lastname, String email, String phonenumber, int account_type, String username, String password)
   {
     try (Connection connection = ConnectionDB.getConnection()) {
-      PreparedStatement statement = connection.prepareStatement("SELECT * from \"Accounts\" where \"username\"=?;");
+      PreparedStatement statement = connection.prepareStatement("SELECT * from sep2schema.account where username=?;");
       statement.setString(1, username);
       ResultSet resultSet = statement.executeQuery();
       if (resultSet.next()) {
         connection.close();
         return "Username is already taken";
       } else {
-        PreparedStatement statement2 = connection.prepareStatement("INSERT INTO \"Accounts\"(\"username\", \"password\", \"firstname\", \"lastname\", \"email\", \"phonenumber\") VALUES (?,?,?,?,?,?);");
-        statement2.setString(1, username);
-        statement2.setString(2, password);
-        statement2.setString(3, firstname);
-        statement2.setString(4, lastname);
-        statement2.setString(5, email);
-        statement2.setString(6, phonenumber);
+        PreparedStatement statement2 = connection.prepareStatement("INSERT INTO sep2schema.account(\"firstname\", \"lastname\", \"email\", \"phonenumber\", \"account_type\", \"username\", \"password\") VALUES (?,?,?,?,?,?,?);");
+        statement2.setString(1, firstname);
+        statement2.setString(2, lastname);
+        statement2.setString(3, email);
+        statement2.setString(4, phonenumber);
+        statement2.setInt(5, account_type);
+        statement2.setString(5, username);
+        statement2.setString(6, password);
         statement2.executeUpdate();
         connection.close();
 
